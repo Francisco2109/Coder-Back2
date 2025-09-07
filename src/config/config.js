ccf04590d6ae9b672f74ca9cbb5f942aae8ac13c
Francisco <fggp09@gmail.com>
2025-09-07 16:54:42 -0300

    AAAA

--- src/config/config.js
+++ src/config/config.js
@@ -4,7 +4,5 @@ export default {
   PORT: process.env.PORT,
   MONGO_URL: process.env.MONGO_URL,
   SECRET_KEY: process.env.SECRET_KEY,
-  JWT_SECRET: NULL,
-  CLIENT_ID_GITHUB: process.env.CLIENT_ID_GITHUB,
-  CLIENT_SECRET_GITHUB: process.env.CLIENT_SECRET_GITHUB,
+  JWT_SECRET: process.env.JWT_SECRET,
 };
